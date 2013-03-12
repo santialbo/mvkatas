bf38ce2571e1895da803374adc21ac520dc18c56
santi <santialbo@gmail.com>
2013-03-12 01:58:45 -0400

    Added kata Levenshtein paths

---
+++ leveshtein/Program.fs
@@ -0,0 +1,73 @@
+
+module leveshtein.Main
+
+open System
+open System.Collections.Generic 
+
+open System.Collections.Generic 
+
+let chars = [|'a'..'z'|]
+
+let Levenshtein (a: array<char>) (b: array<char>) =
+    let cache = Dictionary<_, _>()
+    let rec lev i j =
+        if cache.ContainsKey(i, j) then cache.[i, j]
+        else
+            let x =
+                match i, j with
+                | 0, j -> j
+                | i, 0 -> i
+                | _, _ -> min (min (lev (i - 1) j + 1) (lev i (j - 1) + 1)) (lev (i - 1) (j - 1) + (if a.[i - 1] = b.[j - 1] then 0 else 1))
+            cache.Add((i, j), x)
+            x
+    in lev a.Length b.Length
+
+let add i c a = Seq.concat [Seq.take i a; Seq.singleton c; Seq.skip i a] |> Seq.toArray
+    
+let delete i a = Seq.concat [Seq.take i a; Seq.skip (i + 1) a] |> Seq.toArray
+    
+let change i c a = Seq.concat [Seq.take i a; Seq.singleton c; Seq.skip (i + 1) a] |> Seq.toArray
+
+type 'a Tree = Nil | Tree of 'a * list<'a Tree>
+
+let EditChain a b =
+    let rec generate a =
+        let d = Levenshtein a b
+        let gens = seq {
+            for i in 0..(a.Length - 1) do
+                let c1 = delete i a
+                if (Levenshtein c1 b) < d then yield c1 else ()
+                for c in chars do
+                    let c2 = add i c a
+                    if (Levenshtein c2 b) < d then yield c2 else ()
+                    let c3 = change i c a
+                    if (Levenshtein c3 b) < d then yield c3 else ()
+            }
+        seq {
+            for gen in gens do
+            yield Tree(gen, generate gen)
+         } |> Seq.toList
+    let c = generate a
+    in Tree(a, c) 
+
+let rec Flatten = function
+    | Nil -> [[]]
+    | Tree(a, []) -> [[a]]
+    | Tree(a, ts) ->
+        let ts2 = ts |> List.map(fun t -> Flatten t) |> List.concat
+        in ts2 |> List.map (fun t -> a::t)
+
+let PrettyPrint (t: char array list list) =
+    let pca (a: char array) = new System.String(a)
+    t |> List.iter (fun cw ->
+        printf "%s" (pca (List.head cw))
+        (List.tail cw) |> List.iter (fun w -> printf " -> %s" (pca w))
+        printfn "")
+                    
+[<EntryPoint>]
+let main args =
+    let a = Console.ReadLine().ToCharArray() 
+    let b = Console.ReadLine().ToCharArray()
+    PrettyPrint (Flatten (EditChain a b))   
+    0
+
